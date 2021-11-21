33995c80c293bbf9723a5b67b891c804c7c1dd67
Jimmy Wärting <jimmy@warting.se>
2021-11-21 20:34:31 +0100

    replace depr. querystring \w URLSearchParams
    ... among other things

--- build.js
+++ build.js
@@ -1,13 +1,14 @@
 #!/usr/bin/env node
 
-var fs = require('fs');
-var path = require('path');
-var querystring = require('querystring');
-var child_process = require('child_process');
+const fs = require('node:fs');
+const path = require('node:path');
+const child_process = require('node:child_process');
+const https = require('node:https');
+const { text } = require('node:stream/consumers');
 
-var browserify = path.resolve(path.join('node_modules', '.bin', 'browserify'));
-var webpack = path.resolve(path.join('node_modules', '.bin', 'webpack'));
-var coffee = path.resolve(path.join('node_modules', '.bin', 'coffee'));
+const browserify = path.resolve(path.join('node_modules', '.bin', 'browserify'));
+const webpack = path.resolve(path.join('node_modules', '.bin', 'webpack'));
+const coffee = path.resolve(path.join('node_modules', '.bin', 'coffee'));
 
 function run(command, callback) {
   console.log(command);
@@ -16,90 +17,90 @@ function run(command, callback) {
 
 // Use browserify to package up source-map-support.js
 fs.writeFileSync('.temp.js', 'sourceMapSupport = require("./source-map-support");');
-run(browserify + ' .temp.js', function(error, stdout) {
+
+run(browserify + ' .temp.js', (error, stdout) => {
   if (error) throw error;
 
   // Wrap the code so it works both as a normal <script> module and as an AMD module
-  var header = [
+  const header = [
     '/*',
     ' * Support for source maps in V8 stack traces',
     ' * https://github.com/evanw/node-source-map-support',
     ' */',
   ].join('\n');
-  var code = [
+
+  const code = [
     '(this["define"] || function(name, callback) { this["sourceMapSupport"] = callback(); })("browser-source-map-support", function(sourceMapSupport) {',
     stdout.replace(/\bbyte\b/g, 'bite').replace(new RegExp(__dirname + '/', 'g'), '').replace(/@license/g, 'license'),
     'return sourceMapSupport});',
   ].join('\n');
 
   // Use the online Google Closure Compiler service for minification
-  var body = Buffer.from(querystring.stringify({
+  const body = new URLSearchParams({
     compilation_level: 'SIMPLE_OPTIMIZATIONS',
     output_info: 'compiled_code',
     output_format: 'text',
     js_code: code
-  }));
+  });
+
+  const buffer = new TextEncoder().encode(body.toString())
+
   console.log('making request to google closure compiler');
-  var https = require('https');
-  var request = https.request({
+
+  const request = https.request({
     method: 'POST',
     host: 'closure-compiler.appspot.com',
     path: '/compile',
     headers: {
-      'content-length': body.length,
+      'content-length': buffer.byteLength,
       'content-type': 'application/x-www-form-urlencoded'
     },
   });
-  request.end(body);
-  request.on('response', function(response) {
-    if (response.statusCode !== 200) {
-      response.pipe(process.stderr);
-      response.on('end', function() {
-        throw new Error('failed to post to closure compiler');
-      });
-      return;
-    }
 
-    var data = [];
-    response.on('data', function(chunk) {
-      data.push(chunk);
-    });
-    response.on('end', function() {
-      var stdout = Buffer.concat(data);
-      var code = header + '\n' + stdout;
+  request.once('response', response => {
+    text(response).then(stdout => {
       fs.unlinkSync('.temp.js');
+
+      if (response.statusCode !== 200) {
+        console.error(stdout);
+        throw new Error('failed to post to closure compiler');
+      }
+
+      const code = header + '\n' + stdout;
       fs.writeFileSync('browser-source-map-support.js', code);
       fs.writeFileSync('amd-test/browser-source-map-support.js', code);
     });
   });
+
+  request.end(buffer);
 });
 
 // Build the AMD test
-run(coffee + ' --map --compile amd-test/script.coffee', function(error) {
+run(coffee + ' --map --compile amd-test/script.coffee', error => {
   if (error) throw error;
 });
 
 // Build the browserify test
-run(coffee + ' --map --compile browserify-test/script.coffee', function(error) {
+run(coffee + ' --map --compile browserify-test/script.coffee', error => {
   if (error) throw error;
-  run(browserify + ' --debug browserify-test/script.js > browserify-test/compiled.js', function(error) {
+  run(browserify + ' --debug browserify-test/script.js > browserify-test/compiled.js', error => {
     if (error) throw error;
   })
 });
 
 // Build the browser test
-run(coffee + ' --map --compile browser-test/script.coffee', function(error) {
+run(coffee + ' --map --compile browser-test/script.coffee', error => {
   if (error) throw error;
 });
 
 // Build the header test
-run(coffee + ' --map --compile header-test/script.coffee', function(error) {
+run(coffee + ' --map --compile header-test/script.coffee', error => {
   if (error) throw error;
-  var contents = fs.readFileSync('header-test/script.js', 'utf8');
+  const contents = fs.readFileSync('header-test/script.js', 'utf8');
   fs.writeFileSync('header-test/script.js', contents.replace(/\/\/# sourceMappingURL=.*/g, ''))
 });
 
 // Build the webpack test
-child_process.exec(webpack, {cwd: 'webpack-test'}, function(error) {
+child_process.exec(webpack, {cwd: 'webpack-test'}, error => {
   if (error) throw error;
 });
